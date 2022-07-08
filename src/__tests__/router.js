f27f580a6558f176208ca2485c697ff3578adc5c
Anthony <aamadia24@gmail.com>
2022-07-08 11:32:41 -0400

    Update router.js

--- src/__tests__/router.js
+++ src/__tests__/router.js
@@ -4,17 +4,9 @@ import React, { Provider } from 'react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history';
 import AppRouter from '../router'
-// import 'jsdom-worker' - removed this package but may reinstall later.
 
-// todo: fix ReferenceError: Worker is not defined
 
 test('AppRouter renders all routes and I can navigate to those pages', () => {
-    render(
-        <Provider>
-            <Router history={createMemoryHistory()}>
-                <AppRouter />
-            </Router>
-        </Provider>
-    )
+    render(<AppRouter />)
 })
 
